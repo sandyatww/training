9d5208060a8873599b47f73412b7661e02513024
Gowda Seema <seemag05@gmail.com>
2021-08-26 14:28:40 +1000

    First Commit

--- Solid/Solid/Program.cs
+++ Solid/Solid/Program.cs
@@ -7,6 +7,7 @@ namespace Solid
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Seema Gowda");
         }
     }
 }
